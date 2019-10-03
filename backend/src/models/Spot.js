665dc20145f1b8376958e4c5c151d350f5659687
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2019-10-03 16:55:13 -0300

    Initial commit

---
+++ backend/src/models/Spot.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const SpotSchema = new Schema(
+  {
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://10.13.5.71:3333/files/${this.thumbnail}`;
+});
+
+module.exports = model("Spot", SpotSchema);
---
+++ mobile/src/pages/List.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  AsyncStorage,
+  StyleSheet
+} from "react-native";
+
+import SpotList from "../components/SpotList";
+
+import logo from "../assets/logo.png";
+
+export default function List({ navigation }) {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storagedTechs => {
+      const techsArray = storagedTechs.split(",").map(tech => tech.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  async function handleLogout() {
+    await AsyncStorage.clear();
+
+    navigation.navigate("Login");
+  }
+
+  return (
+    <SafeAreaView style={StyleSheet.container}>
+      <TouchableOpacity onPress={handleLogout}>
+        <Image style={styles.logo} source={logo} />
+      </TouchableOpacity>
+
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 50
+  }
+});
